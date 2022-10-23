091c05b2da42ebba8394178bcda66f7e8d680154
Yulu Xu <xuyulu86@126.com>
2022-10-23 22:45:03 +0800

    add snake

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,103 +17,190 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snake'),
+      ),
+      body: const SnakeGame(),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+const double size = 10;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+enum Direction {
+  up,
+  down,
+  left,
+  right,
+}
+
+class SnakeGame extends StatefulWidget {
+  const SnakeGame({Key? key}) : super(key: key);
+
+  @override
+  State<SnakeGame> createState() => _SnakeGameState();
+}
+
+class _SnakeGameState extends State<SnakeGame> {
+  Offset _ball = Offset.zero;
+  List<Offset> _snake = [
+    const Offset(50, 0),
+    const Offset(60, 0),
+  ];
+  Direction _snakeDirection = Direction.left;
+  late Timer _timer;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final maxWidth = _getMaxWidth();
+    final maxHeight = _getMaxHeight();
+    _timer = Timer.periodic(
+      const Duration(milliseconds: 200),
+      (timer) {
+        List<Offset> newOffset = _updateSnakePosition(maxWidth, maxHeight);
+        Offset newBall = _ball;
+        if (newOffset[0] == _ball) {
+          newOffset.add(_snake[_snake.length - 1]);
+          newBall = _randomBall(maxWidth.toInt(), maxHeight.toInt());
+        }
+        setState(() {
+          _snake = newOffset;
+          _ball = newBall;
+        });
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    List<Widget> snake = _getSnake();
+    Widget ball = _getBall();
+    snake.add(ball);
+    return RawKeyboardListener(
+      focusNode: FocusNode(),
+      autofocus: true,
+      onKey: _onKey,
+      child: Stack(
+        children: snake,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+
+  Offset _randomBall(int widthRange, int heightRange) {
+    var random = Random();
+    var nextX = _getFitPosition(random.nextInt(widthRange).toDouble());
+    var nextY = _getFitPosition(random.nextInt(heightRange).toDouble());
+    return Offset(nextX, nextY);
+  }
+
+  double _getMaxWidth() {
+    return _getFitPosition(MediaQuery.of(context).size.width);
+  }
+
+  double _getMaxHeight() {
+    return _getFitPosition(MediaQuery.of(context).size.height);
+  }
+
+  double _getFitPosition(double newPosition) {
+    double widthPad = newPosition % size;
+    return newPosition - widthPad;
+  }
+
+  void _onKey(RawKeyEvent? event) {
+    if (event.runtimeType == RawKeyDownEvent) {
+      Direction newDirection = _snakeDirection;
+      switch (event?.logicalKey.keyLabel) {
+        case "Arrow Down":
+          newDirection = Direction.down;
+          break;
+        case "Arrow Up":
+          newDirection = Direction.up;
+          break;
+        case "Arrow Left":
+          newDirection = Direction.left;
+          break;
+        case "Arrow Right":
+          newDirection = Direction.right;
+          break;
+      }
+      setState(() {
+        _snakeDirection = newDirection;
+      });
+    }
+  }
+
+  List<Offset> _updateSnakePosition(double maxWidth, double maxHeight) {
+    return List.generate(_snake.length, (index) {
+      if (index > 0) {
+        return _snake[index - 1];
+      }
+      final snakeHead = _snake[0];
+      switch (_snakeDirection) {
+        case Direction.left:
+          return Offset(
+              (snakeHead.dx - size + maxWidth) % maxWidth, snakeHead.dy);
+        case Direction.right:
+          return Offset((snakeHead.dx + size) % maxWidth, snakeHead.dy);
+        case Direction.up:
+          return Offset(
+              snakeHead.dx, (snakeHead.dy - size + maxHeight) % maxHeight);
+        case Direction.down:
+          return Offset(snakeHead.dx, (snakeHead.dy + size) % maxHeight);
+      }
+    });
+  }
+
+  List<Widget> _getSnake() {
+    return _snake
+        .map(
+          (item) => Positioned.fromRect(
+            rect: Rect.fromCenter(
+              center: _adjust(item),
+              width: size,
+              height: size,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            child: Container(
+              margin: const EdgeInsets.all(1),
+              color: Colors.black,
             ),
-          ],
-        ),
+          ),
+        )
+        .toList();
+  }
+
+  Widget _getBall() {
+    return Positioned.fromRect(
+      rect: Rect.fromCenter(
+        center: _adjust(_ball),
+        width: size,
+        height: size,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      child: Container(
+        margin: const EdgeInsets.all(1),
+        color: Colors.orange,
+      ),
+    );
+  }
+
+  Offset _adjust(Offset offset) {
+    return Offset(
+      offset.dx + size / 2,
+      offset.dy + size / 2,
     );
   }
 }

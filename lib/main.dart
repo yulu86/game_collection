b90518c92cf4d3feb038c57d9adf4822d344cfe1
Yulu Xu <xuyulu86@126.com>
2022-10-23 20:41:20 +0800

    update pingpong

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:game_collection/src/ball.dart';
+import 'package:game_collection/src/ball_state.dart';
+import 'package:game_collection/src/rocket.dart';
+import 'package:game_collection/src/start_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,105 +18,119 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PingPong',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+const double rocketStep = 0.1;
+const double ballStep = 0.02;
+const double maxY = 0.95;
+const double minY = -0.95;
+const rocketLength = 1 / 3;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _MyHomePageState extends State<MyHomePage> {
+  double _upRocketX = 0;
+  double _downRocketX = 0;
+  bool _isRunning = false;
+  final BallState _ballState = BallState();
+  late Timer _timer;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.white,
+      body: RawKeyboardListener(
+        focusNode: FocusNode(),
+        autofocus: true,
+        onKey: _onKey,
+        child: SafeArea(
+          child: GestureDetector(
+            onTap: _startGame,
+            child: Stack(
+              children: [
+                // 启动界面
+                StartScreen(isShow: _isRunning),
+                // 顶部的球拍
+                Rocket(x: _upRocketX, y: minY, color: Colors.red),
+                // 乒乓球
+                Ball(x: _ballState.ballX, y: _ballState.ballY),
+                // 底部的球拍
+                Rocket(x: _downRocketX, y: maxY, color: Colors.black),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _startGame() {
+    setState(() => _isRunning = true);
+    _timer = Timer.periodic(
+      const Duration(milliseconds: 50),
+      (timer) => _updateBall(),
+    );
+  }
+
+  void _updateBall() {
+    setState(() {
+      _ballState.update();
+      bool isCollision = _ballState.checkCollision(_upRocketX, _downRocketX);
+      if (isCollision) {
+        _stopGame();
+      }
+    });
+  }
+
+  void _stopGame() {
+    _isRunning = false;
+    _timer.cancel();
+    setState(_reset);
+  }
+
+  void _reset() {
+    _upRocketX = 0;
+    _downRocketX = 0;
+    _ballState.reset();
+  }
+
+  void _onKey(RawKeyEvent? event) {
+    if (event.runtimeType == RawKeyDownEvent) {
+      final keyLabel = event?.logicalKey.keyLabel;
+      double newDownRocketX = _downRocketX;
+      double newUpRocketX = _upRocketX;
+      if (_ballState.vDirection == Direction.down) {
+        newDownRocketX = _getNewRocketX(_downRocketX, keyLabel);
+      } else if (_ballState.vDirection == Direction.up) {
+        newUpRocketX = _getNewRocketX(_upRocketX, keyLabel);
+      }
+
+      setState(() {
+        _downRocketX = newDownRocketX;
+        _upRocketX = newUpRocketX;
+      });
+    }
+  }
+
+  double _getNewRocketX(double oldRocketX, String? keyLabel) {
+    if (keyLabel == "Arrow Left") {
+      return oldRocketX - rocketStep;
+    } else if (keyLabel == "Arrow Right") {
+      return oldRocketX + rocketStep;
+    }
+    return oldRocketX;
   }
 }
---
+++ lib/src/ball_state.dart
@@ -0,0 +1,85 @@
+import 'package:game_collection/main.dart';
+
+enum Direction {
+  up,
+  down,
+  left,
+  right,
+}
+
+class BallState {
+  double ballX;
+  double ballY;
+  Direction vDirection;
+  Direction hDirection;
+
+  BallState({
+    this.ballX = 0,
+    this.ballY = 0,
+    this.vDirection = Direction.down,
+    this.hDirection = Direction.right,
+  });
+
+  void update() {
+    ballX = _getNewBallX();
+    ballY = _getNewBallY();
+    hDirection = _getNewHDirection();
+    vDirection = _getNewVDirection();
+  }
+
+  bool checkCollision(double upRocketX, double downRocketX) {
+    if (ballY <= minY) {
+      return ballX < (upRocketX - rocketLength) ||
+          ballX > (upRocketX + rocketLength);
+    }
+
+    if (ballY >= maxY) {
+      return ballX < (downRocketX - rocketLength) ||
+          ballX > (downRocketX + rocketLength);
+    }
+
+    return false;
+  }
+
+  void reset() {
+    ballX = 0;
+    ballY = 0;
+    vDirection = Direction.down;
+    hDirection = Direction.right;
+  }
+
+  double _getNewBallY() {
+    if (vDirection == Direction.down) {
+      return ballY + ballStep;
+    }
+    return ballY - ballStep;
+  }
+
+  double _getNewBallX() {
+    const step = ballStep * 2;
+    if (hDirection == Direction.right) {
+      return ballX + step;
+    }
+    return ballX - step;
+  }
+
+  Direction _getNewVDirection() {
+    if (ballY <= minY) {
+      return Direction.down;
+    } else if (ballY >= maxY) {
+      return Direction.up;
+    } else {
+      return vDirection;
+    }
+  }
+
+  Direction _getNewHDirection() {
+    if (ballX <= -1) {
+      return Direction.right;
+    } else if (ballX >= 1) {
+      return Direction.left;
+    } else {
+      return hDirection;
+    }
+  }
+}

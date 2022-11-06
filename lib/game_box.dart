a3d0d9727a0729f5ac9931d255e716927c5746cc
Yulu Xu <xuyulu86@126.com>
2022-11-06 18:09:48 +0800

    绘制白色方块

--- lib/game_box.dart
+++ lib/game_box.dart
@@ -13,9 +13,27 @@ class BoxGame extends Game {
 
   @override
   void render(Canvas canvas) {
-    var bgRect = Rect.fromLTWH(0, 0, screenSize.length, screenSize.length2);
+    // 屏幕宽度和高度
+    double screenWidth = screenSize.x;
+    double screenHeight = screenSize.y;
+
+    // 绘制黑色背景
+    var bgRect = Rect.fromLTWH(0, 0, screenWidth, screenHeight);
     Paint bgPaint = Paint()..color = const Color(0xff000000);
     canvas.drawRect(bgRect, bgPaint);
+
+    // 绘制白色方块
+    double screenCenterX = screenWidth / 2;
+    double screenCenterY = screenHeight / 2;
+    double size = 150;
+    Rect boxRect = Rect.fromLTWH(
+      screenCenterX - size / 2,
+      screenCenterY - size / 2,
+      size,
+      size,
+    );
+    Paint boxPaint = Paint()..color = const Color(0xffffffff);
+    canvas.drawRect(boxRect, boxPaint);
   }
 
   @override
